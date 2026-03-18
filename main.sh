0eaf117810c0bd59be3d8f316d695c7b703ff745
muriel <muriel@bewise.com.es>
2026-03-18 12:27:16 -0300

    Desarrollo la opcion 6 y mejoro visualmente el menu

--- main.sh
+++ main.sh
@@ -10,22 +10,29 @@ export FILENAME="datos-estudiantes"
 
 
 crear_entorno() {
-  mkdir -p $DIRECTORIO $DIRECTORIO_ENTRADA $DIRECTORIO_SALIDA $DIRECTORIO_PROCESADO
+  mkdir -p $DIRECTORIO_ENTRADA $DIRECTORIO_SALIDA $DIRECTORIO_PROCESADO
 }
 
 menu() {
-  echo "Seleccione una opción:"
-  echo "1) Crear entorno"
-  echo "2) Correr proceso"
-  echo "3) mostrar listado de alumnos ordenados por numero de padon"
-  echo "4) Mostrar las 10 notas mas altas"
-  echo "5) Alumno por padron"
-  echo "6) Salir"
+  echo "┌─────────────────────────────────────┐"
+  echo "│                MENU                 │"
+  echo "├─────────────────────────────────────┤"
+  echo "│ 1 - Crear entorno                   │"
+  echo "│ 2 - Correr proceso                  │"
+  echo "│ 3 - Listado de alumnos              │"
+  echo "│ 4 - Mostrar las 10 notas mas altas  │"
+  echo "│ 5 - Buscar alumno por padron        │"
+  echo "│ 6 - Salir                           │"
+  echo "└─────────────────────────────────────┘"
+  echo -n "Selecione una opcion: "
   read x
   case $x in
     1) crear_entorno ;;
     2) ./EPNro1/consolidar.sh & ;;
-    6)  $SALIDA=true;;
+    3);;
+    4);;
+    5);;
+    6)  SALIDA=true;;
     *) echo "Opción no válida" ;;
   esac
 }
