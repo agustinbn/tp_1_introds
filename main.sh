fb1eaa7272aa45f42c973a8dd8300b048d263525
muriel <muriel@bewise.com.es>
2026-03-17 23:50:11 -0300

    creacion del menu y avance en la opcion 2

--- main.sh
+++ main.sh
@@ -4,10 +4,37 @@ DIRECTORIO="./EPNro1"
 DIRECTORIO_ENTRADA="$DIRECTORIO/entrada"
 DIRECTORIO_SALIDA="$DIRECTORIO/salida"
 DIRECTORIO_PROCESADO="$DIRECTORIO/procesado"
+CONSOLIDAR= '
+FILENAME="archivo"
+
+cat entrada/*.txt >> salida/$FILENAME.txt
+mv entrada/*.txt procesado/'
 
 export FILENAME="datos-estudiantes"
 
+
 crear_entorno() {
   mkdir -p $DIRECTORIO $DIRECTORIO_ENTRADA $DIRECTORIO_SALIDA $DIRECTORIO_PROCESADO
-  touch $DIRECTORIO_SALIDA/$FILENAME.txt
+  touch $DIRECTORIO
+  cat $CONSOLIDAR > $DIRECTORIO/consolidar.sh
 }
+
+menu() {
+  echo "Seleccione una opción:"
+  echo "1) Crear entorno"
+  echo "2) Correr proceso"
+  echo "3) mostrar listado de alumnos ordenados por numero de padon"
+  echo "4) Mostrar las 10 notas mas altas"
+  echo "5) Alumno por padron"
+  echo "6) Salir"
+  read x
+  case $x in
+    1) crear_entorno ;;
+    2) consolidar.sh ;;
+    3)  ;;
+    *) echo "Opción no válida" ;;
+  esac
+}
+
+
+crear_entorno
