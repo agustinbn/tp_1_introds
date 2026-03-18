fb7c90b0796e0f987290aeb92978b7cac63293b5
Agustín Bianchi <bianchiagus6@gmail.com>
2026-03-18 12:33:41 -0300

    add option 4 and some minor fixes

--- main.sh
+++ main.sh
@@ -1,16 +1,25 @@
 #! /bin/bash
 
+FILENAME="archivo"
 DIRECTORIO="./EPNro1"
 DIRECTORIO_ENTRADA="$DIRECTORIO/entrada"
 DIRECTORIO_SALIDA="$DIRECTORIO/salida"
 DIRECTORIO_PROCESADO="$DIRECTORIO/procesado"
-SALIDA=false
-
-export FILENAME="datos-estudiantes"
-
 
 crear_entorno() {
-  mkdir -p $DIRECTORIO $DIRECTORIO_ENTRADA $DIRECTORIO_SALIDA $DIRECTORIO_PROCESADO
+  mkdir -p $DIRECTORIO_ENTRADA $DIRECTORIO_SALIDA $DIRECTORIO_PROCESADO
+}
+
+correr_proceso() {
+	bash $DIRECTORIO/consolidar.sh &
+}
+
+mostrar_notas_altas() {
+	if [ -f "$DIRECTORIO_SALIDA/$FILENAME.txt" ]; then
+		cat "$DIRECTORIO_SALIDA/$FILENAME.txt" | sort -k 5 -n -r | head -n 10
+	else
+		echo "El archivo $FILENAME.txt no existe en el directorio $DIRECTORIO_SALIDA"
+	fi
 }
 
 menu() {
@@ -24,11 +33,13 @@ menu() {
   read x
   case $x in
     1) crear_entorno ;;
-    2) ./EPNro1/consolidar.sh & ;;
-    6)  $SALIDA=true;;
+    2) correr_proceso ;;
+		4) mostrar_notas_altas ;;
+    6) exit 0 ;;
     *) echo "Opción no válida" ;;
   esac
 }
-until [ "$SALIDA" = "true" ]; do
+
+while true; do
   menu
 done
