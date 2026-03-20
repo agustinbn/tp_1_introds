4e499f606bd30e7fa9f9100c0485fdffc21e646e
Muriel Soiffer <soiffermuriel@gmail.com>
2026-03-20 20:13:49 -0300

    modifico consolidar.sh para que funcione sin necesidad del for para revisar todos los archivos .txt dentro de la carpeta entrada.

--- consolidar.sh
+++ consolidar.sh
@@ -7,17 +7,10 @@ DIRECTORIO_SALIDA="$DIRECTORIO/salida"
 DIRECTORIO_PROCESADO="$DIRECTORIO/procesado"
 
 while true ; do 
-    for archivo in "$DIRECTORIO_ENTRADA"/*.txt; do
-        if [ -f "$archivo" ]; then          
-
-            cat "$archivo" >> "$DIRECTORIO_SALIDA/$FILENAME.txt"
-
-            sort -u "$DIRECTORIO_SALIDA/$FILENAME.txt" -o "$DIRECTORIO_SALIDA/$FILENAME.txt"
-
-            mv "$archivo" "$DIRECTORIO_PROCESADO/"
-
-        fi
-
-    done
+    if [ -n "$(ls $DIRECTORIO_ENTRADA/*.txt 2>/dev/null | head -n 1)" ]; then
+        cat "$DIRECTORIO_ENTRADA"/*.txt >> "$DIRECTORIO_SALIDA/$FILENAME.txt"
+        sort -u "$DIRECTORIO_SALIDA/$FILENAME.txt" -o "$DIRECTORIO_SALIDA/$FILENAME.txt"
+        mv "$DIRECTORIO_ENTRADA"/*.txt "$DIRECTORIO_PROCESADO/"
+    fi
     sleep 5
 done
